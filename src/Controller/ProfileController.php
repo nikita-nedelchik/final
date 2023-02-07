eab0c02e16230b8eff575c203f0e19164745bbd8
nikita <nedelka999@gmail.com>
2023-02-07 17:16:10 +0300

    separate user and admin parts, made profile page

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -5,12 +5,16 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class ProfileController extends AbstractController
 {
     #[Route(path: '/profile', name: 'profile_index')]
-    public function profile(): Response
+    public function profile(UserInterface $user): Response
     {
-        return $this->render('/profile/index.html.twig');
+        return $this->render('/profile/index.html.twig', [
+            'user' => $user,
+            'isAdmin' => in_array('ROLE_ADMIN', $user->getRoles())
+        ]);
     }
 }
